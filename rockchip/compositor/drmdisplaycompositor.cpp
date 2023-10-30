3f144586442849f2a23edb9e4320128f90f33aa2
libin <bin.li@rock-chips.com>
2023-10-30 21:58:55 +0800

    Perf-opt : Reduce std::__1::basic_ostream construction behavior.
    Change-Id: Ic7f545e4e15b93bfc02076ba692155baeb41d0a4

--- rockchip/compositor/drmdisplaycompositor.cpp
+++ rockchip/compositor/drmdisplaycompositor.cpp
@@ -1222,21 +1222,23 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
     size_t index=0;
     std::ostringstream out_log;
 
-    out_log << "DrmDisplayCompositor[" << index << "]"
-            << " frame_no=" << display_comp->frame_no()
-            << " display=" << display_comp->display()
-            << " plane=" <<  plane->name()
-            << " crct id=" << crtc->id()
-            << " fb id=" << fb_id
-            << " display_frame[" << dst_l << ","
-            << dst_t << "," << dst_w
-            << "," << dst_h << "]"
-            << " source_crop[" << src_l << ","
-            << src_t << "," << src_w
-            << "," << src_h << "]"
-            << ", zpos=" << zpos
-            ;
-    index++;
+    if(LogLevel(DBG_DEBUG)){
+      out_log << "DrmDisplayCompositor[" << index << "]"
+              << " frame_no=" << display_comp->frame_no()
+              << " display=" << display_comp->display()
+              << " plane=" <<  plane->name()
+              << " crct id=" << crtc->id()
+              << " fb id=" << fb_id
+              << " display_frame[" << dst_l << ","
+              << dst_t << "," << dst_w
+              << "," << dst_h << "]"
+              << " source_crop[" << src_l << ","
+              << src_t << "," << src_w
+              << "," << src_h << "]"
+              << ", zpos=" << zpos
+              ;
+      index++;
+    }
 
     if (plane->rotation_property().id()) {
       ret = drmModeAtomicAddProperty(pset, plane->id(),
@@ -1247,7 +1249,9 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
               plane->rotation_property().id(), plane->id());
         break;
       }
-      out_log << " rotation=" << rotation;
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " rotation=" << rotation;
+      }
     }
 
     if (plane->alpha_property().id()) {
@@ -1258,7 +1262,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
               plane->alpha_property().id(), plane->id());
         break;
       }
-      out_log << " alpha=" << std::hex <<  alpha;
+
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " alpha=" << std::hex <<  alpha;
+      }
     }
 
     if (plane->blend_property().id()) {
@@ -1269,7 +1276,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
               plane->blend_property().id(), plane->id());
         break;
       }
-      out_log << " blend mode =" << blend;
+
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " blend mode =" << blend;
+      }
     }
 
     if(plane->get_hdr2sdr() && plane->eotf_property().id()) {
@@ -1281,7 +1291,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
               plane->eotf_property().id(), plane->id());
         break;
       }
-      out_log << " eotf=" << std::hex <<  eotf;
+
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " eotf=" << std::hex <<  eotf;
+      }
     }
 
     if(gIsDrmVerison6_1()){
@@ -1294,7 +1307,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
                 plane->kernel6_1_color_encoding().id(), plane->id());
           break;
         }
-        out_log << " color_encoding=" << std::hex <<  colorspace.colorspace_kernel_6_1_.color_encoding_;
+
+        if(LogLevel(DBG_DEBUG)){
+          out_log << " color_encoding=" << std::hex <<  colorspace.colorspace_kernel_6_1_.color_encoding_;
+        }
       }
 
       if(plane->kernel6_1_color_range().id()) {
@@ -1306,7 +1322,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
                 plane->kernel6_1_color_range().id(), plane->id());
           break;
         }
-        out_log << " color_range=" << std::hex <<  colorspace.colorspace_kernel_6_1_.color_range_;
+
+        if(LogLevel(DBG_DEBUG)){
+          out_log << " color_range=" << std::hex <<  colorspace.colorspace_kernel_6_1_.color_range_;
+        }
       }
     }else{
       if(plane->colorspace_property().id()) {
@@ -1318,7 +1337,10 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
                 plane->colorspace_property().id(), plane->id());
           break;
         }
-        out_log << " colorspace=" << std::hex <<  colorspace.colorspace_kernel_510_;
+
+        if(LogLevel(DBG_DEBUG)){
+          out_log << " colorspace=" << std::hex <<  colorspace.colorspace_kernel_510_;
+        }
       }
     }
 
@@ -1332,12 +1354,17 @@ int DrmDisplayCompositor::CollectCommitInfo(drmModeAtomicReqPtr pset,
         break;
       }
 
-      out_log << " async_commit=" << sideband;
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " async_commit=" << sideband;
+      }
     }
 
     // Next hdr base layer
     if(is_metadata_hdr) {
-      out_log << " is_metadata_hdr=" << is_metadata_hdr;
+
+      if(LogLevel(DBG_DEBUG)){
+        out_log << " is_metadata_hdr=" << is_metadata_hdr;
+      }
     }
 
     HWC2_ALOGD_IF_DEBUG("%s",out_log.str().c_str());
