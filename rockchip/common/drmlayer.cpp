6d1a164b70ba76a84dc45570c1e64d37d64f4984
libin <bin.li@rock-chips.com>
2024-03-15 15:54:45 +0800

    BugFix : ResetInfoFromStore missed resetting the HeightStride.
    Signed-off-by: libin <bin.li@rock-chips.com>
    Change-Id: I4efd4cabcb953427ea6d750b64b079ab5f40b122

--- rockchip/common/drmlayer.cpp
+++ rockchip/common/drmlayer.cpp
@@ -1014,6 +1014,7 @@ void DrmHwcLayer::ResetInfoFromStore(){
   iWidth_      = storeLayerInfo_.iWidth_ ;
   iHeight_     = storeLayerInfo_.iHeight_;
   iStride_     = storeLayerInfo_.iStride_;
+  iHeightStride_ = storeLayerInfo_.iHeightStride_;
   iByteStride_ = storeLayerInfo_.iByteStride_;
   iUsage       = storeLayerInfo_.iUsage;
   uFourccFormat_ = storeLayerInfo_.uFourccFormat_;
@@ -1026,9 +1027,9 @@ void DrmHwcLayer::ResetInfoFromStore(){
 
   Init();
   HWC2_ALOGD_IF_DEBUG(
-             "reset:DrmHwcLayer[%4u] Buffer[w/h/s/format]=[%4d,%4d,%4d,%4d] Fourcc=%c%c%c%c Transform=%-8.8s(0x%x) Blend[a=%d]=%-8.8s "
+             "reset:DrmHwcLayer[%4u] Buffer[w/h/s/hs/format]=[%4d,%4d,%4d,%4d,%4d] Fourcc=%c%c%c%c Transform=%-8.8s(0x%x) Blend[a=%d]=%-8.8s "
              "source_crop[l,t,r,b]=[%5.0f,%5.0f,%5.0f,%5.0f] display_frame[l,t,r,b]=[%4d,%4d,%4d,%4d],skip=%d,afbcd=%d,gles=%d\n",
-             uId_,iWidth_,iHeight_,iStride_,iFormat_,uFourccFormat_,uFourccFormat_>>8,uFourccFormat_>>16,uFourccFormat_>>24,
+             uId_,iWidth_,iHeight_,iStride_,iHeightStride_,iFormat_,uFourccFormat_,uFourccFormat_>>8,uFourccFormat_>>16,uFourccFormat_>>24,
              TransformToString(transform).c_str(),transform,alpha,BlendingToString(blending).c_str(),
              source_crop.left,source_crop.top,source_crop.right,source_crop.bottom,
              display_frame.left,display_frame.top,display_frame.right,display_frame.bottom,bSkipLayer_,bAfbcd_,bGlesCompose_);
