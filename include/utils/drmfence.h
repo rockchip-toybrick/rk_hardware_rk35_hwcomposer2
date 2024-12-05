e272b1209bb2f88811fb12a89ea2658ae68563a3
libin <bin.li@rock-chips.com>
2024-12-05 15:26:55 +0800

    BugFix : Fix the ReleaseFence reporting error problem.(Fix)
    Redmine : Defect #519838
    
    Change-Id: Ifcf5d87b4c4bbaa65063c3032abdc34f205c24d7
    Signed-off-by: libin <bin.li@rock-chips.com>

--- include/utils/drmfence.h
+++ include/utils/drmfence.h
@@ -182,8 +182,10 @@ class DeferredReleaseFence {
             mFences.pop();
         }
 
+        // 根据 hwcomposer2.h 接口要求，Layer::GetReleaseFence
+        // 应该获取当前帧的ReleaseFence，而不是获取上一帧
         const sp<ReleaseFence> &get() const {
-            return mFences.front();
+            return mFences.back();
         }
 
         const sp<ReleaseFence> &get_back() const {
